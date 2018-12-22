0023d5a7e5f02bd0363ad64c11403e9e37f8f950
Carolina <carolinalaginestra@hotmail.com>
2018-12-22 17:20:38 -0200

    feat: creating homepage and routes

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 import './App.css';
+import HomePage from './HomePage';
+
 
 class App extends Component {
   render() {
     return (
-      <Container>
+      <BrowserRouter>
+        <Container>
 
-      </Container>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+          </Switch>
+
+        </Container>
+      </BrowserRouter>
     );
   }
 }
